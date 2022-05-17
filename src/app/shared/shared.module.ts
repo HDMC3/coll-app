d3ac4acb4b2185f03858209d9856305ee62f7d26
Hugo Moran <moranh56@gmail.com>
2022-05-17 02:28:22 -0600

    Add pipe to truncate text in descriptions

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmModalComponent } from './components/confirm-modal/confirm-modal.component';
+import { MaxLengthPipe } from './pipes/max-length.pipe';
 
 @NgModule({
     declarations: [
-        ConfirmModalComponent
+        ConfirmModalComponent,
+        MaxLengthPipe
     ],
     imports: [
         CommonModule
     ],
     exports: [
-        ConfirmModalComponent
+        ConfirmModalComponent,
+        MaxLengthPipe
     ]
 })
 export class SharedModule { }
