135caf5cdbf4967e9fc1301d6389435764f307e9
Hugo Moran <moranh56@gmail.com>
2022-05-18 12:46:27 -0600

    Add task detail modal component

---
+++ src/app/modules/pages/tasks/components/task-detail-modal/task-detail-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-detail-modal',
+  templateUrl: './task-detail-modal.component.html',
+  styleUrls: ['./task-detail-modal.component.scss']
+})
+export class TaskDetailModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/pages/tasks/tasks.module.ts
+++ src/app/modules/pages/tasks/tasks.module.ts
@@ -7,13 +7,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewTaskModalComponent } from './components/new-task-modal/new-task-modal.component';
 import { EditTaskModalComponent } from './components/edit-task-modal/edit-task-modal.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { TaskDetailModalComponent } from './components/task-detail-modal/task-detail-modal.component';
 
 
 @NgModule({
     declarations: [
         TasksComponent,
         NewTaskModalComponent,
-        EditTaskModalComponent
+        EditTaskModalComponent,
+        TaskDetailModalComponent
     ],
     imports: [
         CommonModule,
