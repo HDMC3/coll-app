aa95290bc667ee209e9dabd7d1eb8ed9cb8e0d46
Hugo Moran <moranh56@gmail.com>
2022-05-19 09:35:11 -0600

    Add not found page component

---
+++ src/app/shared/components/not-found-page/not-found-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-not-found-page',
+    templateUrl: './not-found-page.component.html',
+    styleUrls: ['./not-found-page.component.scss']
+})
+export class NotFoundPageComponent {
+
+    constructor(private router: Router) { }
+
+    backTo() {
+        this.router.navigate(['app', 'tasks']);
+    }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmModalComponent } from './components/confirm-modal/confirm-modal.component';
 import { MaxLengthPipe } from './pipes/max-length.pipe';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
     declarations: [
         ConfirmModalComponent,
-        MaxLengthPipe
+        MaxLengthPipe,
+        NotFoundPageComponent
     ],
     imports: [
         CommonModule
